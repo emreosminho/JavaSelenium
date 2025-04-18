e5f9e74d9877c6ae4d2806728faf3a6a790f0219
emreosminho <emreosmancanakci@gmail.com>
2025-04-18 13:15:37 +0300

    manage() methodu ücüncü bölüm

---
+++ src/test/java/lesson_4/DriverManageMethods3.java
@@ -0,0 +1,17 @@
+package lesson_4;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManageMethods3 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Project\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // 1.Amazon sayfasına gidelim
+        driver.get("https://www.amazon.com/");
+
+    }
+}
