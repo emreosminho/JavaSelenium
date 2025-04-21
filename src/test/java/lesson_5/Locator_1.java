4435e35699df46243b25c2e28601ac09bae8c937
emreosminho <emreosmancanakci@gmail.com>
2025-04-21 12:38:37 +0300

    locators 1

---
+++ src/test/java/lesson_5/Locator_1.java
@@ -0,0 +1,22 @@
+package lesson_5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locator_1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Project\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // https://www.amazon.com.tr/ git
+        driver.get("https://www.amazon.com.tr/");
+
+        // Çerezleri kabul et
+        driver.findElement(By.id(    "sp-cc-accept")).click();
+
+
+
+    }
+}
