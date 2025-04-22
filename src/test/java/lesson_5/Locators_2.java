8b29cc4bd4973934d7118a5e2457e53f42296d40
emreosminho <emreosmancanakci@gmail.com>
2025-04-22 16:00:57 +0300

    locators_2

---
+++ src/test/java/lesson_5/Locators_2.java
@@ -0,0 +1,25 @@
+package lesson_5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators_2 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Project\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.amazon.com.tr/");
+        driver.findElement(By.id("sp-cc-accept")).click();
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("laptop");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+        String sonucSayisi = driver.findElement(By.xpath("//div[@class=\"sg-col-inner\"]/h2/span")).getText();
+        System.out.println(sonucSayisi);
+
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
